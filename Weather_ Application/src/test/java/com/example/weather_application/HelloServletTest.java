635b08fff4e9e9220c8f9c341ef79df8041199bb
Tinson <107073910+mtinsonk@users.noreply.github.com>
2023-11-20 14:24:35 -0600

    Monikai Added backend files to new repo

---
+++ Weather_ Application/src/test/java/com/example/weather_application/HelloServletTest.java
@@ -0,0 +1,30 @@
+package com.example.weather_application;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloServletTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void destroy() {
+    }
+}
